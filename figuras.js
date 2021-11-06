9a049f472dde27c2d8c3e6d14031dd11155b6d3a
Alberto Ola <alberto.ola.dev@gmail.com>
2021-11-06 00:46:01 -0600

    created a new function to calculate the height of a isoceles triangle

--- figuras.js
+++ figuras.js
@@ -15,11 +15,24 @@ console.groupEnd();
 console.group("Triangulos");
 
 function perimetroTriangulo(lado1, lado2, base) {
-    return lado1 + lado2 + base;
+    return parseFloat(lado1) + parseFloat(lado2) + parseFloat(base);
 }
 
 function areaTriangulo(base, altura) {
     return (base * altura) / 2;
+}
+
+function alturaTrianguloIsoceles(lado1, lado2, base) {
+
+    if (!(lado1 > 0 && lado2 > 0 && base > 0)) return null;
+
+    if (lado1 !== lado2) return null;
+
+    const mitadDeLaBase = base / 2;
+    
+    const altura = Math.sqrt(Math.pow(lado1, 2) - Math.pow(mitadDeLaBase / 2, 2));
+
+    return altura;
 }
 
 console.groupEnd();
@@ -47,18 +60,72 @@ console.groupEnd();
 
 // Aquí interactuamos con el HTML
 
-function calcularPerimetroCuadrado(params) {
+// Cuadrado
+
+function calcularPerimetroCuadrado() {
     const input = document.getElementById("InputCuadrado");
     const value = input.value;
     
     const perimetro = perimetroCuadrado(value);
-    alert(perimetro);
+
+    
+    if (perimetro > 0) alert(perimetro);
 }
 
-function calcularAreaCuadrado(params) {
+function calcularAreaCuadrado() {
     const input = document.getElementById("InputCuadrado");
     const value = input.value;
     
     const area = areaCuadrado(value);
-    alert(area);
+    if (area > 0) alert(area);
 }
+
+// Triángulo
+
+function calcularPerimetroTriangulo() {
+    const input1 = document.getElementById("InputTrianguloLado1");
+    const lado1 = input1.value;
+
+    const input2 = document.getElementById("InputTrianguloLado2");
+    const lado2 = input2.value;
+
+    const input3 = document.getElementById("InputTrianguloBase");
+    const base = input3.value;
+    
+    const perimetro = perimetroTriangulo(lado1, lado2, base);
+
+    
+    if (perimetro > 0) alert(perimetro);
+}
+
+function calcularAreaTriangulo() {
+    const input1 = document.getElementById("InputTrianguloBase");
+    const base = input1.value;
+
+    const input2 = document.getElementById("InputTrianguloAltura");
+    const altura = input2.value;
+    
+    const area = areaTriangulo(base, altura);
+    if (area > 0) alert(area);
+}
+
+// Circulo
+
+function calcularPerimetroCirculo() {
+    const input = document.getElementById("InputCirculo");
+    const value = input.value;
+    
+    const perimetro = perimetroCirculo(value);
+
+    
+    if (perimetro > 0) alert(perimetro);
+}
+
+function calcularAreaCirculo() {
+    const input = document.getElementById("InputCirculo");
+    const value = input.value;
+    
+    const area = areaCirculo(value);
+    if (area > 0) alert(area);
+}
+
